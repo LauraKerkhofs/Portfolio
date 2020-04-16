8c1d07c424b6268e00cf6b9d9105effc12fb16e1
Laura Kerkhofs <kerkhofslaura@hotmail.com>
2020-04-16 17:02:38 +0200

    Sidebar
    - Sidebar > Icos
    - Sidebar > I am

--- js/js-code.js
+++ js/js-code.js
@@ -63,7 +63,38 @@ var main = function () {
 		$(".grid").removeClass("gridAndSidebar")
 	});
 };
-
 $(document).ready(main);
 
+
+
+var nameArray = ["Webdesigner", "Camera Operator", "Video Editor", "Photo Manipulator", "3D Enthusiast", "Audio Cleaner", "Learner",
+					 "Perfectionist",
+					  "Singalong Artist", "Open Book", "Gardener", "Food Lover",
+					  "King of the World", "Sunflower", "Rainbow", ""
+					 ]; //One of those will be randomly chosen
+
+var lastIndex = 0; //Used to remember the last word picked
+
+//This function will get a random INT used later as the array index
+function getRandomInt(iMin, iMax) {
+    var newIndex = Math.floor(Math.random() * (iMax - iMin)) + iMin;
+    while( lastIndex == newIndex ){
+        var newIndex = Math.floor(Math.random() * (iMax - iMin)) + iMin;
+    }
+  lastIndex = newIndex;  
+
+  return newIndex;
+}
+
+function pickRandomName() {
+    var domHeaderName = document.getElementById("change"); //Put the ID of the DOM element you want to change here
+    var randomIndex = getRandomInt(0, nameArray.length);
+
+    //Put the randomly chosen name in the DOM
+    domHeaderName.innerHTML = nameArray[randomIndex];
+    //loop
+    setTimeout(pickRandomName, 2000); //Loop every 2000ms (2s)
+}
+
+pickRandomName();
 
