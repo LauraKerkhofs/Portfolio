f8bbfac539795c2e328d9395429f3db4120ddd9b
Laura Kerkhofs <kerkhofslaura@hotmail.com>
2020-04-16 11:40:56 +0200

    Restyling
    First of serveral adjustments for restyling

--- js/js-code.js
+++ js/js-code.js
@@ -1,133 +1,4 @@
 /*///////////////////////////////////////*/
-/*//////// SWIPER PAGINA SLIDEN /////////*/
-/*///////////////////////////////////////*/
-var swiperH = new Swiper('.swiper-container-h', {
-	spaceBetween: 20,
-	hashNavigation: {
-		watchState: true,
-	},
-	grabCursor: true,
-	keyboard: {
-		enabled: true,
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	}
-});
-var swiperV = new Swiper('.swiper-container-v', {
-	direction: 'vertical',
-	slidesPerView: 'auto',
-	spaceBetween: 5,
-	freeMode: true,
-	grabCursor: true,
-	mousewheel: true,
-	scrollbar: {
-		el: '.swiper-scrollbar',
-	},
-	keyboard: {
-		enabled: true,
-	}
-});
-
-
-
-
-
-
-
-
-
-
-/*///////////////////////////////////////*/
-/*//////// PARTICLES HOME PAGINA ////////*/
-/*///////////////////////////////////////*/
-particlesJS("particles-js", {
-	"particles": {
-		"number": {
-			"value": 40,
-			"density": {
-				"enable": true,
-				"value_area": 400
-			}
-		},
-		"color": {"value": "#64b29a"},
-		"shape": {
-			"type": "circle",
-			"stroke": {
-				"width": 0,
-				"color": "#000000"
-			}
-		},
-		"opacity": {
-			"value": 0.3,
-			"random": true,
-			"anim": {
-				"enable": true,
-				"speed": 0,
-				"opacity_min": 0.1,
-				"sync": false
-			}
-		},
-		"size": {
-			"value": 5,
-			"random": true,
-			"anim": {
-				"enable": true,
-				"speed": 5,
-				"size_min": 0.5,
-				"sync": false
-			}
-		},
-		"line_linked": {"enable": false},
-		"move": {
-			"enable": true,
-			"speed": 1,
-			"direction": "none",
-			"random": true,
-			"straight": false,
-			"out_mode": "out",
-			"bounce": false,
-			"attract": {
-				"enable": true,
-				"rotateX": 600,
-				"rotateY": 1200
-			}
-		}
-	},
-	"interactivity": {
-		"detect_on": "canvas",
-		"events": {
-			"onhover": {
-				"enable": false,
-				"mode": "repulse"
-			},
-			"onclick": {
-				"enable": false,
-				"mode": "push"
-			},
-			"resize": true
-		},
-		"modes": {
-			"repulse": {
-				"distance": 40,
-				"duration": 0.4
-			},
-			"push": {"particles_nb": 10},
-		}
-	}
-});
-
-
-
-
-
-
-
-
-
-
-/*///////////////////////////////////////*/
 /*/////// ISOTOPE PROJECTS PAGINA ///////*/
 /*///////////////////////////////////////*/
 var $grid = $('.grid').isotope({
@@ -136,7 +7,7 @@ var $grid = $('.grid').isotope({
 	stagger: 50,
 	percentPosition: true,
 	masonry: {
-		columnWidth: '.grid-sizer'
+		columnWidth: '.afbeeldingen'
 	}
 });
 
@@ -172,4 +43,27 @@ $filters = $('#filters').on( 'click', 'button', function() {
 
 	$grid.isotope({ filter: filterValue });
 });
+
+
+
+/*///////////////////////////////////////*/
+/*/////////////// SIDEBAR ///////////////*/
+/*///////////////////////////////////////*/
+var main = function () {
+	"use strict";
+	
+	$(".logoBlackOlive").on("click", function() {
+		$(".sidebarInfo").removeClass("onzichtbaar");
+		$(".logoBlackOlive").addClass("onzichtbaar");
+		$(".grid").addClass("gridAndSidebar")
+	});
+	$(".logoWhiteSmoke").on("click", function() {
+		$(".sidebarInfo").addClass("onzichtbaar");
+		$(".logoBlackOlive").removeClass("onzichtbaar");
+		$(".grid").removeClass("gridAndSidebar")
+	});
+};
+
+$(document).ready(main);
+
 
